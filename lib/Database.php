74d92ec8b36c5ab2b75481c5bccc68dec106c9e7
Seydel, Daniel <dseydel@united-internet.de>
2022-04-12 08:51:31 +0200

    Initialize

---
+++ lib/Database.php
@@ -0,0 +1,65 @@
+<?php
+
+class Database
+{
+
+    private $config;
+    private $connection;
+
+    public function __construct()
+    {
+        $json = file_get_contents('../configs/mysql.json');
+        $this->config = json_decode($json, true);
+    }
+
+    /**
+     * @return bool
+     */
+    public function connect()
+    {
+        $this->connection = mysqli_connect($this->config['hostname'], $this->config['username'],
+            $this->config['password'], $this->config['database'], $this->config['port']);
+        if(!$this->connection){
+            return false;
+        }
+        return true;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    /**
+     * @return bool
+     */
+    public function is_connected()
+    {
+        return $this->connection != null;
+    }
+
+    /**
+     * @return bool
+     */
+    public function update($statement)
+    {
+        if(!$this->is_connected()){
+            $this->connect();
+        }
+
+        return mysqli_query($this->connection, $statement);
+    }
+
+    /**
+     * @return bool
+     */
+    public function query($statement)
+    {
+        if(!$this->is_connected()){
+            $this->connect();
+        }
+
+        return mysqli_query($this->connection, $statement);
+    }
+
+}
